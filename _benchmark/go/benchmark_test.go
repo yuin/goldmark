e7353e629948cadfafa37045197f6947e085f42f
Andy Hochhaus <ahochhaus@samegoal.com>
2021-01-10 14:42:39 -0600

    Use canonical blackfriday v2 import path
    https://github.com/russross/blackfriday/issues/587

--- _benchmark/go/benchmark_test.go
+++ _benchmark/go/benchmark_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/yuin/goldmark/util"
 	"gitlab.com/golang-commonmark/markdown"
 
-	bf2 "gopkg.in/russross/blackfriday.v2"
+	"github.com/russross/blackfriday/v2"
 
 	"github.com/88250/lute"
 )
@@ -19,7 +19,7 @@ import (
 func BenchmarkMarkdown(b *testing.B) {
 	b.Run("Blackfriday-v2", func(b *testing.B) {
 		r := func(src []byte) ([]byte, error) {
-			out := bf2.Run(src)
+			out := blackfriday.Run(src)
 			return out, nil
 		}
 		doBenchmark(b, r)
